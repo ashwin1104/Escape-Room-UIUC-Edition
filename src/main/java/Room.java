aa8d0f085953fb0c63351069bbf4ad4d90cd110c
ashwin1104 <ashwin.saxena1@gmail.com>
2019-09-23 23:59:46 -0500

    Updated constructors, getters, and setters for each class to include new instance fields.

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -6,22 +6,44 @@ public class Room {
     private ArrayList<Direction> directions;
     private ArrayList<Item> items;
 
-    public Room(String name, String description, ArrayList<Direction> directions) {
+    public Room(String name, String description, ArrayList<Direction> directions, ArrayList<Item> items) {
         this.name = name;
         this.description = description;
         this.directions = directions;
+        this.items = items;
     }
 
-    // Getters
+    // Getters and Setters
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public ArrayList<Direction> getDirections() {
         return directions;
+    }
+
+    public void setDirections(ArrayList<Direction> directions) {
+        this.directions = directions;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
     }
 }
