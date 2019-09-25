da7777bbd2f5f662fc358d898585f92c78d9a200
ashwin1104 <ashwin.saxena1@gmail.com>
2019-09-24 23:15:15 -0500

    Added ability to use command line to input URL or file path.

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,11 +1,31 @@
 import com.google.gson.Gson;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Adventure {
+    private static boolean isURL = false;
+    private static boolean isFile = false;
+
+    public static boolean isURL() {
+        return isURL;
+    }
+
+    public static void setIsURL(boolean isURL) {
+        Adventure.isURL = isURL;
+    }
+
+    public static boolean isFile() {
+        return isFile;
+    }
+
+    public static void setIsFile(boolean isFile) {
+        Adventure.isFile = isFile;
+    }
+
     // This function reads JSON from url and returns the JSON as a String
     // Source: https://stackoverflow.com/questions/4328711/read-url-to-string-in-few-lines-of-java-code
     public static String readFromURL(String requestURL) throws IOException {
@@ -16,23 +36,28 @@ public class Adventure {
             return scanner.hasNext() ? scanner.next() : "";
         }
     }
-
     // This method handles user input for URL, sending out a default url (our given JSON url for this assignment)
     // If the user's url is bad or if they don't have one
     // Otherwise, returns the user's url
-    public static String getURLFromUser() {
-        Scanner userResponse = new Scanner(System.in);
-        System.out.println("Which URL would you like to use for your Adventure game?");
-        System.out.println("If you have no URL, say \"None\"");
-
-        String url = userResponse.nextLine();
-        if (url == null || url.equalsIgnoreCase("None") || !isURLValid(url)) {
-            System.out.println("You either specified no URL or an incorrect URL.");
-            System.out.println("Default URL will be used for your Adventure game.");
+    public static String getInputFromUser(String input) {
+        if (input == null) {
+            System.out.println("You have specified a null file path or URL.");
+            System.out.println("Default file will be used for your Adventure game.");
             System.out.println("");
-            return "https://courses.grainger.illinois.edu/cs126/fa2019/assignments/siebel.json";
+            return "src/adventure.json";
         }
-        return url;
+        else if (isURLValid(input)) {
+            return input;
+        }
+        else if (isFileValid(input)) {
+            return input;
+        }
+        else {
+            System.out.println("You either specified an incorrect URL or an incorrect file path.");
+            System.out.println("Default file will be used for your Adventure game.");
+            System.out.println("");
+            return "src/adventure.json";
+        }
     }
 
     // Source: https://www.geeksforgeeks.org/check-if-url-is-valid-or-not-in-java/
@@ -40,6 +65,7 @@ public class Adventure {
     public static boolean isURLValid(String url) {
         try {
             new URL(url).toURI();
+            setIsURL(true);
             return true;
         }
         catch (Exception e) {
@@ -47,9 +73,46 @@ public class Adventure {
         }
     }
 
+    //
+    public static boolean isFileValid(String file) {
+        File tmpFile = new File(file);
+        if (tmpFile.exists()) {
+            setIsFile(true);
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) throws IOException {
-        // read in JSON file into String variable
-        String myJSON = Data.getFileContents("src", "adventure.json");
+        System.out.println("Which URL would you like to use for your Adventure game?" +
+                " Or, enter the file path of a local JSON file");
+        System.out.println("If you have no URL and no path, say \"None\"");
+        System.out.println("");
+
+        String myJSON;
+        String input = "";
+        if (args.length == 0) {
+            myJSON = Data.getFileContents("src", "adventure.json");
+        }
+        else {
+            input = getInputFromUser(args[0]);
+        }
+
+        if (isURL()) {
+            System.out.println("Your url is valid and will now be used");
+            myJSON =
+                    readFromURL(input);
+        }
+        else if (isFile()) {
+            System.out.println("Your file is valid and will now be used");
+            String firstPath = input.substring(0,input.lastIndexOf("/"));
+            String secondPath = input.substring(input.lastIndexOf("/"));
+            myJSON = Data.getFileContents(firstPath, secondPath);
+        }
+        else {
+            // read in JSON file into String variable
+            myJSON = Data.getFileContents("src", "adventure.json");
+        }
+
         Gson gson = new Gson();
         // read in String variable of JSON file into network of class declarations
         Layout layout = gson.fromJson(myJSON, Layout.class);
